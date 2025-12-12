b1e91659ccda887af582a3738170eae257b1f1ac
josephstayton36-commits <josephstayton36@gmail.com>
2025-12-11 18:14:53 -0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,15 +1,21 @@
 // 🔥 Your Firebase config (from Firebase Console)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBK_o7S1Bcp5u4hKA3j-V_xodrD6PxtNCQ",
+  authDomain: "ai-credit-repair-992c7.firebaseapp.com",
+  projectId: "ai-credit-repair-992c7",
+  storageBucket: "ai-credit-repair-992c7.firebasestorage.app",
+  messagingSenderId: "411411517046",
+  appId: "1:411411517046:web:fa2013a0f914aa06fc5957"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-
+const app = initializeApp(firebaseConfig);
 // LOGIN FUNCTION
 function loginUser() {
   const email = document.getElementById("email").value;
