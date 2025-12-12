e5a542027b4d1545acfbf40949984b849c7789d1
josephstayton36-commits <josephstayton36@gmail.com>
2025-12-12 17:32:39 -0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+// firebase-config.js
+// IMPORTANT: Paste YOUR Firebase config object below:
+
+const firebaseConfig = {
+  apiKey: "PASTE_YOURS",
+  authDomain: "PASTE_YOURS",
+  projectId: "PASTE_YOURS",
+  appId: "PASTE_YOURS"
+  // If your config includes storageBucket, messagingSenderId, etc. include them too.
+};
+
+firebase.initializeApp(firebaseConfig);
